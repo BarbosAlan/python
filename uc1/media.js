3df8437a75ba1f65266e0343069666b36383c3b3
Alan Barbosa <149962269+BarbosAlan@users.noreply.github.com>
2025-01-24 16:42:54 -0300

    Update final aula dia 24/01/2025
    final da aula

---
+++ uc1/media.js
@@ -0,0 +1,15 @@
+let nota1 = 10
+let nota2 = 10
+
+
+let media = (nota1 + nota2) /2
+
+if (media >= 8) {
+    console.log("Conceito Bom")
+}
+else if (media >= 6)
+    console.log("Conceito Regular")
+else 
+{
+    console.log("Conceito insuficiente")
+}
---
+++ uc1/times.js
@@ -0,0 +1,8 @@
+let time = "Botafogo"
+
+if (time == "Botafogo"){
+    console.log("Melhor time")
+} 
+else 
+    {console.log("não é o Fogão")
+}
